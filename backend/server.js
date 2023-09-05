da65fe19b9081f4d304b42588509ef155fc59027
Rigoberto González <themackintux@gmail.com>
2023-09-04 20:19:33 -0600

    actualizando - los puertos del backend (node)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
-app.listen(5000, () => {
+app.listen(3000, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
